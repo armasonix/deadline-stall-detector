ad90ac566727d8442fe9d3b874e6ffa2c18b001b
armasonix <74196072+armasonix@users.noreply.github.com>
2026-06-29 22:33:00 +0300

    test: expect standard release version label

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -3,4 +3,4 @@ from deadline_tools.version import APP_VERSION, APP_VERSION_LABEL
 
 def test_version_label_uses_central_project_version():
     assert APP_VERSION == "1.1.7"
-    assert APP_VERSION_LABEL == "v.1.1.7"
+    assert APP_VERSION_LABEL == "v1.1.7"
